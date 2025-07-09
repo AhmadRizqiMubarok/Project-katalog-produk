804d8a4a039660e46e5eef04c8aacfaf39f62293
AhmadRizqiMubarok <khususcadanginwa@gmail.com>
2025-07-09 14:11:22 +0700

    menu

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -25,6 +25,9 @@
         <li class="nav-item">
           <a class="nav-link" href="#">Menu 2</a>
         </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Menu 3</a>
+        </li>
       </ul>
 
       <ul class="navbar-nav ms-auto">
