fb01af002b48d5ce0e1ddc21b6d2e8b728a56150
AhmadRizqiMubarok <khususcadanginwa@gmail.com>
2025-07-09 13:49:21 +0700

    tryy

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,8 +11,8 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container">
     <a class="navbar-brand fw-bold fs-3 text-uppercase" href="{{ url('/') }}">
-  Roti Sari
-</a>
+      Roti Sari
+    </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarBasic">
       <span class="navbar-toggler-icon"></span>
     </button>
